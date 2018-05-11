1db43f4a2f753af42f328a4c4fc01be125b1d63f
Oanh Harry <oanhharry@MacBook-Air-cua-Nguyen.local>
2018-05-11 12:46:21 +0700

    final

--- Web1/NEW-FLASH-APP.py
+++ Web1/NEW-FLASH-APP.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 @app.route ('/')
 def index():
-    return "school"
+    return "Hi C4E17"
+
+@app.route ('/about-me')
+def hi():
+    return "Hello, I'm Oanh, 25 years olds. I'm a marketer. Travelling is my hobby"
+
+@app.route ('/school')
+def school():
+    return redirect('http://techkids.vn/')
 
 
 if __name__ == '__main__':
